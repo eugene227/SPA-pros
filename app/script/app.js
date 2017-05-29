5386eff2d5f06549365fe16d2ae051a9d256a207
eugene227 <myclasses@mail.com>
2017-05-29 14:35:45 -0700

    updating app.js and main.js

--- app/script/app.js
+++ app/script/app.js
@@ -1,38 +1,52 @@
+
 function openAccordions(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
+    var element = document.getElementById(id);
+    //if class exists then remove it and return false, if not, then add it and return true.
+	element.classList.toggle("w3-show") ? element.classList.toggle("w3-hide") : element.classList.toggle("w3-hide");
 } //end of function
+
+
+
+
 function openTab(evt, tabName) {
     var i, x, tablinks;
     //display no content of any tab
-    x = document.getElementsByClassName("city");
+    x = document.getElementsByClassName("homeTabs");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
+
     //don't have any red bars under the tabs
     tablinks = document.getElementsByClassName("tablink");
+
     for (i = 0; i < x.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
     }
+  
     //show the content of the clicked tab
     document.getElementById(tabName).style.display = "block";
     //add the class "w3-border-red" to make the tab bar a red color
     evt.currentTarget.firstElementChild.className += " w3-border-red";
 } //end of function
+
+
+
+
+
 function openReviewTab(evt, tabName) {
     var i, x;
     //display no content of any tab
-    x = document.getElementsByClassName("city");
+    x = document.getElementsByClassName("reviewTabs");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     } //end of loop
     //show the content of the clicked tab
     document.getElementById(tabName).style.display = "block";
 } //end of function
+
+
+
+
 function openSurveyItems(evt, tabName) {
     var i, x;
     //display no content of any tab
@@ -43,9 +57,16 @@ function openSurveyItems(evt, tabName) {
     //show the content of the clicked tab
     document.getElementById(tabName).style.display = "block";
 } //end of function
+
+
+
+
 function w3_open(id) {
     document.getElementById(id).style.display = "block";
 } //end of function
+
+
+
 function w3_close(id) {
     document.getElementById(id).style.display = "none";
 } //end of function
--- app/script/main.js
+++ app/script/main.js
@@ -5,15 +5,10 @@ STATE["purpose"] = "SPA state should be kept in STATE object";
 //=================================================================
 PAGE["landing"] = "#landing";
 CLICK("#landing .X_login", function(json, status) { // CHECK
-    hashUnset("registration");
-    hashSet("login");
+    hashUpdate("login");
 });
 CLICK("#landing .X_register", function(json, status) {
-    hashUnset("login");
-    hashSet("registration");
-});
-CLICK("#landing .X_reviews", function(json, status) {
-    hashUpdate("landing|reviews");
+    hashUpdate("registration");
 });
 //=================================================================
 //  LOGIN
@@ -97,6 +92,164 @@ CLICK("#reviews .X_____", function(json, status) {
             break;
     }
 });
+//=================================================================
+//  mySurveys - home/dashboard
+//=================================================================
+PAGE["mySurveys"] = "#mySurveys";
+CLICK("#mySurveys .X_mySurveys", function(json, status) { // CHECK
+    hashUpdate("mySurveys");
+});
+CLICK("#mySurveys .X_reviews", function(json, status) {
+    hashUpdate("reviews");
+});    
+CLICK("#mySurveys .X_logout", function(json, status) {
+    hashUpdate("landing");
+});
+CLICK("#mySurveys .X_create-survey", function(json, status) {
+    hashUpdate("create-survey");
+});
+CLICK("#mySurveys .X_edit-survey", function(json, status) {
+    hashUpdate("edit-survey");
+});
+//=================================================================
+//  create-survey
+//=================================================================
+PAGE["create-survey"] = "#create-survey";
+CLICK("#create-survey .X_mySurveys", function(json, status) { // CHECK
+    hashUpdate("mySurveys");
+});
+CLICK("#create-survey .X_reviews", function(json, status) {
+    hashUpdate("reviews");
+});    
+CLICK("#create-survey .X_logout", function(json, status) {
+    hashUpdate("landing");
+});
+CLICK("#create-survey .X_cancle", function(json, status) {
+    hashUpdate("mySurveys");
+});
+CLICK("#create-survey .X_create-preview", function(json, status) {
+    // pout("#login .X_login");
+    switch (status) {
+        case "exec":
+            // pout("exec");
+            
+            json["title"] = IVAL("#create-survey .I_title");
+            json["description"] = IVAL("#create-survey .I_description");           
+            
+            //FIXME what if one of the survey items are styled as "display:none" in the DOM, will they also be included in the loop?
+            
+            //select all elements with classes I_question1 and I_choiceList1
+            //create a json property for each question and choice list, name them properlly
+            var questions = document.querySelector("#create-survey .question");
+            var choiceList = document.querySelector("#create-survey .choiceList");
+            var statusPublic = document.querySelector("#create-survey .statusPublic");
+            
+            //loop through each question and choiceList to add proper class names
+            for (var i = 0; i < questions.length; i++) {
+            	
+            	//adding I_question[i] class to each question
+            	questions[i].classList.add("I_question" + i);	
+            	//adding question to json
+				json["question"+i] = IVAL("#create-survey .I_question" + i);
+            	
+            	//adding I_statusPublic[i] class to each checkbox
+            	statusPublic[i].classList.add("I_statusPublic" + i);	
+            	//adding statusPublic to json
+				json["statusPublic"+i] = IVAL("#create-survey .I_statusPublic" + i);
+
+            	
+             	//adding I_choiceList[i] class to each question
+            	choiceList[i].classList.add("I_choiceList" + i);	
+            	//adding choiceList to json
+				json["choiceList"+i] = IVAL("#create-survey .I_choiceList" + i);
+           	
+            }//end of for loop
+            
+            SEND(json);
+            break;
+        case "success":
+            pout("Survey Created");
+            pout("GO HOME");
+            // GO("home");
+            break;
+        case "failure":
+            for (message of json["MESSAGE"]) {
+                pout(message);
+            }
+            pout("Failed to delete survey");
+            break;
+    }
+});
+//=================================================================
+//  edit-survey
+//=================================================================
+PAGE["edit-survey"] = "#edit-survey";
+CLICK("#edit-survey .X_mySurveys", function(json, status) { // CHECK
+    hashUpdate("mySurveys");
+});
+CLICK("#edit-survey .X_reviews", function(json, status) {
+    hashUpdate("reviews");
+});    
+CLICK("#edit-survey .X_logout", function(json, status) {
+    hashUpdate("landing");
+});
+CLICK("#edit-survey .X_cancle", function(json, status) {
+    hashUpdate("mySurveys");
+});
+CLICK("#edit-survey .X_save-changes", function(json, status) {
+    // pout("#login .X_login");
+    switch (status) {
+        case "exec":
+            // pout("exec");
+            
+            json["title"] = IVAL("#edit-survey .I_title");
+            json["description"] = IVAL("#edit-survey .I_description");           
+            
+            //FIXME what if one of the survey items are styled as "display:none" in the DOM, will they also be included in the loop?
+            
+            //select all elements with classes I_question1 and I_choiceList1
+            //create a json property for each question and choice list, name them properlly
+            var questions = document.querySelector("#edit-survey .question");
+            var choiceList = document.querySelector("#edit-survey .choiceList");
+            var statusPublic = document.querySelector("#edit-survey .statusPublic");
+            
+            //loop through each question and choiceList to add proper class names
+            for (var i = 0; i < questions.length; i++) {
+            	
+            	//adding I_question[i] class to each question
+            	questions[i].classList.add("I_question" + i);	
+            	//adding question to json
+				json["question"+i] = IVAL("#edit-survey .I_question" + i);
+            	
+            	//adding I_statusPublic[i] class to each checkbox
+            	statusPublic[i].classList.add("I_statusPublic" + i);	
+            	//adding statusPublic to json
+				json["statusPublic"+i] = IVAL("#edit-survey .I_statusPublic" + i);
+
+            	
+             	//adding I_choiceList[i] class to each question
+            	choiceList[i].classList.add("I_choiceList" + i);	
+            	//adding choiceList to json
+				json["choiceList"+i] = IVAL("#edit-survey .I_choiceList" + i);
+           	
+            }//end of for loop
+            
+            SEND(json);
+            break;
+        case "success":
+            pout("Survey Created");
+            pout("GO HOME");
+            // GO("home");
+            break;
+        case "failure":
+            for (message of json["MESSAGE"]) {
+                pout(message);
+            }
+            pout("Failed to delete survey");
+            break;
+    }
+});
+
 //=================================================================
 // test buttons
 //=================================================================
